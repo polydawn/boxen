b47604f59dbdb2c5f84754f58466bc37c32c9fb5
Nathaniel Kofalt <nathaniel@kofalt.com>
2013-10-10 22:50:47 -0500

    Add web-dev boxen

---
+++ ubuntu/web-dev/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+# Folder layout
+mkdir /service && cd /service
+
+# Prepare apt-get for usage
+. /boxen/scripts/apt-get/update.sh
+
+# Developer tools
+apt-get install -y git
+
+# Node dependancies
+apt-get install -y g++ python2.7 make
+
+# Closure compiler dependancies
+apt-get install -y openjdk-7-jre-headless
+
+# Sass dependancies
+apt-get install -y ruby1.9.1
+
+# Clone node
+git clone https://github.com/joyent/node.git && cd node
+git checkout -q v0.10.20
+
+# Lie about python's location.
+# Despite node's docs they don't follow their own $PYTHON env variable during the makefile.
+ln -s /usr/bin/python2.7 /usr/bin/python
+
+# Build node
+$PYTHON ./configure
+make
+make install
+
+# Install Sass
+gem install sass --no-ri --no-rdoc
+
+# Install less
+npm install -g less
+
+# Common node tools
+npm install -g grunt-cli
+npm install -g bower
+
+# Cleanup
+. /boxen/scripts/apt-get/clean.sh
